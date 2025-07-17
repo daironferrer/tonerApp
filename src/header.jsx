4768965dec7d36fc4032e9cac436e81e36d0bbb4
Dairon Ferrer <dairon.ferrer@purplecrow.com>
2025-07-17 16:42:50 -0400

    5th attempt at image fix

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div class="bg-[#171717] text-white font-[Roboto]">
         <div class="lg:pt-75 lg:pb-75 pt-30 pb-70 grid grid-cols-2 items-center">
             <h1 class="lg:text-4xl text-2xl font-bold m-4">Purple Crow Consumable Inventory</h1>
-            <img src="/ITpurplecrow.jpg" className="size-30 lg:size-42 rounded-md m-4 justify-self-end"/>
+            <img src="/ITpurplecrow.jpg" alt="Purple crow IT" style={{width: '120px', height: '120px'}} />
             <p class="m-4">Tool created to keep track of all company IT consumables</p>
         </div>
         <div class="flex justify-center">
