9d4dc768379136e91a6caf2277ed8910270f12d7
Dairon Ferrer <dairon.ferrer@purplecrow.com>
2025-07-18 14:41:21 -0400

    new styles for image issue

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,11 @@ function Header() {
         <div class="bg-[#171717] text-white font-[Roboto]">
         <div class="lg:pt-75 lg:pb-75 pt-30 pb-70 grid grid-cols-2 items-center">
             <h1 class="lg:text-4xl text-2xl font-bold m-4">Purple Crow Consumable Inventory</h1>
-            <img src="https://toner-app-2.vercel.app/Image.png" className="size-30 lg:size-36 rounded-md m-4 justify-self-end" alt="purple crow IT"/>
+            <img 
+                src="/Image.png" 
+                alt="test"
+                style={{width: '100px', height: '100px'}}
+            />
             <p class="m-4">Tool created to keep track of all company IT consumables</p>
         </div>
         <div class="flex justify-center">
