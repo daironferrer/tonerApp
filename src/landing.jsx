aae7e46f2ee4ac039c282b893a1b4819c06df929
Dairon Ferrer <daironferrer7@gmail.com>
2025-09-25 12:32:56 -0400

    added purple crow logo

--- src/landing.jsx
+++ src/landing.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom"
 function Landing() {
     return(
         <div>
-            <div class="bg-[#171717] text-white font-[Roboto] lg:pt-40 pt-15">
+            <div class="grid grid-cols-1 place-items-center mt-15">
+                <img src="./src/images/PurpleCrow-logo.webp" class="size-50"></img>
+            </div>
+            <div class="bg-[#171717] text-white font-[Roboto] lg:pt-10 pt-15">
             <div class="grid grid-cols-1 place-items-center">
                 <h1></h1>
                 <h1>What consumable would you like to track today?</h1>
